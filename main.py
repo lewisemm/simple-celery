d1b3dd35be24dffa6503dbb9c7f62d371b39e663
Lewis Maina Kabui <maina@Lewiss-MacBook-Pro.local>
2018-05-24 14:14:21 +0300

    Create Synchronous Example

---
+++ main.py
@@ -0,0 +1,9 @@
+import time
+
+print("Let's begin!")
+
+# time.sleep simulates a time consuming section of code that takes
+# 20 seconds to complete.
+time.sleep(20)
+
+print("... and that's the end of our really short app.")
