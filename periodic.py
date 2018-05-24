b4375d4bf9422b27411a9bc9e350bcaf601fcfc3
Lewis Maina Kabui <maina@Lewiss-MacBook-Pro.local>
2018-05-24 23:57:05 +0300

    WIP

---
+++ periodic.py
@@ -0,0 +1,14 @@
+from celery import Celery
+
+app = Celery('periodic', broker="pyamqp://guest@localhost//")
+
+@app.task
+def see_you():
+    print("See you in ten seconds!")
+
+app.conf.beat_schedule = {
+    "see-you-in-ten-seconds-task": {
+        "task": "periodic.see_you",
+        "schedule": 10.0
+    }
+}
